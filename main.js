7c498453058423058ebe0618a3d9368cf4a72000
Julian Atkin <jmd.atkin@gmail.com>
2020-12-02 13:48:38 +0000

    made changes

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 window.onload = function() {
     const canvasEl = document.querySelector(".canvas-wrapper");
-    canvasEl.setAttribute("width",window.innerWidth);
-    canvasEl.setAttribute("height",window.innerHeight);
+
+    const resize = () => {
+    canvasEl.setAttribute("width",`${window.innerWidth}px`);
+    canvasEl.setAttribute("height",`${window.innerHeight}px`);
+    };
+
+    resize();
+    window.addEventListener("resize",resize);
+
     Chem(canvasEl);
 };
  
