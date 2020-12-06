702a22bfa01fad60be26e12c34ffeedf1cba0e32
Julian Atkin <jmd.atkin@gmail.com>
2020-12-06 11:09:47 -0500

    pulled uniforms from chem library and added GUI to modify from webpage

--- main.js
+++ main.js
@@ -10,5 +10,22 @@ window.onload = function() {
     window.addEventListener("resize",resize);
 
     Chem(canvasEl);
+
+
+    Chem.actions.add('a1', () => {
+        Chem.uniforms.set("u_globalTime",Chem.time() / 1000);
+        Chem.uniforms.set("u_offset",Chem.uniforms.get("u_offset").value.add(new THREE.Vector2(
+            0.003,
+            Math.sin(Chem.time()/100)*0.02+0.006)
+        ));
+
+        //Chem.uniforms.set("u_distort",Math.sin(((Math.sin(Chem.time()) * 3) - Chem.time()) / 3));
+    });
+
+    const gui = new dat.GUI({name: 'My GUI'});
+
+    gui.add(Chem.uniforms.get('u_scale'),'value');
+    gui.add(Chem.uniforms.get('u_distort'),'value');
 };
+ 
  
